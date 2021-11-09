6306a13d86c2e3ccf885fa8c22919d8724c033bb
Documenter.jl <documenter@juliadocs.github.io>
2021-11-09 03:32:28 +0000

    build based on 505eaf3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VariationalInequalitySolver","category":"page"},{"location":"#VariationalInequalitySolver","page":"Home","title":"VariationalInequalitySolver","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VariationalInequalitySolver.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VariationalInequalitySolver]","category":"page"}]
+}
